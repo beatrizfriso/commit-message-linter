92a9e0ee547c04afdcfbbd8bf7885a615a85f123
Beatriz Friso <101422055+beatrizfriso@users.noreply.github.com>
2025-04-28 03:02:10 +0000

    first commit

---
+++ src/index.ts
@@ -0,0 +1,33 @@
+import * as core from '@actions/core';
+import { execSync } from 'child_process';
+
+async function run(): Promise<void> {
+  try {
+    const pattern = core.getInput('pattern');
+    const shouldFail = core.getInput('fail') === 'true';
+
+    core.info(`Usando o padrão de commit: ${pattern}`);
+
+    // Busca as mensagens dos commits
+    const commitMessage = execSync('git log -1 --pretty=%B').toString().trim();
+    core.info(`Mensagem do último commit: "${commitMessage}"`);
+
+    const regex = new RegExp(pattern);
+
+    if (!regex.test(commitMessage)) {
+      const message = `❌ A mensagem de commit não segue o padrão: "${pattern}"`;
+
+      if (shouldFail) {
+        core.setFailed(message);
+      } else {
+        core.warning(message);
+      }
+    } else {
+      core.info('✅ A mensagem de commit segue o padrão.');
+    }
+  } catch (error) {
+    if (error instanceof Error) core.setFailed(error.message);
+  }
+}
+
+run();
